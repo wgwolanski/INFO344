a0ccc4b245747eb557cbc168df30c85c87ab87ac
Wesley Wolanski <wgwolanski@gmail.com>
2014-04-14 17:04:47 -0700

    Now working. Need to add stats. Names work

--- search.php
+++ search.php
@@ -1,25 +1,37 @@
-<?php 
-	try {
-
-		$host = "mydb.cogl1pm1ejx2.us-west-2.rds.amazonaws.com:3306";
-		$dbname = "mydb";
-		$user = "wolanwg";
-		$pass = "Blue1234";
-				
-		$mydb = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
-		
-		$search = $_GET['search'];
-		$results = $mydb->prepare("SELECT * FROM NBA WHERE PlayerName LIKE :name");
-		$results->setFetchMode(PDO::FETCH_ASSOC);
-		
-		$results->execute(array(':search' => '%' . $search . '%'));
-		
-		foreach ($results AS $each) {
-			echo $each[0];
-		}
-		
-		$mydb = null;
-	} catch(PDOException $exception) {
-		echo $exception->getMessage();
-	}
+<?php
+try {
+	$host = "mydb.cogl1pm1ejx2.us-west-2.rds.amazonaws.com";
+	$dbname = "mydb";
+	$user = "wolanwg";
+	$pass = "Blue1234";
+	$mydb = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+	
+	$search = $_GET['search'];
+	$stmt = $mydb->prepare("SELECT * FROM NBA WHERE PlayerName LIKE :search");
+	$stmt->execute(array(':search' => '%' . $search . '%'));
+	$result = $stmt->fetchAll();
+} catch(PDOException $e) {
+	echo 'ERROR: ' . $e -> getMessage();
+}
 ?>
+<html>
+	<head>
+		<title>NBA Stats</title>
+		<link rel="stylesheet" href="style.css" />
+	</head>
+	<body>
+		<h1>NBA Player Stat Search</h1>
+		<?php
+		
+		if (count($result)) {
+			foreach ($result as $row) {
+				echo $row['PlayerName'] . '<br>';
+			}
+		} else {
+			echo 'Now players found.';
+		}
+
+		$mydb = null;
+		?>
+	</body>
+</html>
